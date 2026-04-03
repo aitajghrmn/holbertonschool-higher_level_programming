442889625b456e46f4d049f7f84e8150e3c12bc9
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-04-04 01:45:35 +0400

    Simplify ORDER BY clause in SQL query

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
--- List all shows and their genre ids (including NULL)
+-- lists all shows contained in hbtn_0d_tvshows
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
