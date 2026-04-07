12cd74d849a9ca5e628f7ee484898be46b411bab
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-04-07 14:18:32 +0400

    Modify SQL query to use BINARY for case sensitivity

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,10 @@ if __name__ == "__main__":
     )
 
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' "
+        "ORDER BY states.id ASC"
+    )
 
     for row in cur.fetchall():
         print(row)
