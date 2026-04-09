0c9dbfe071e191a7148002e0736b7ea693b569d6
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-04-09 16:05:22 +0400

    Refactor SQL query formatting in my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     )
 
     cur = conn.cursor()
-
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(sys.argv[4])
+    query = "SELECT * FROM states WHERE name = '{}' " \
+            "ORDER BY states.id ASC".format(sys.argv[4])
     cur.execute(query)
 
     for row in cur.fetchall():
