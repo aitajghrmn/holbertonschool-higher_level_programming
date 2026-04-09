a449e6b5c46bf518dff776a33dc3aaf8e53cd5fd
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-04-09 21:00:22 +0400

    Add database connection and query for cities by state

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -10,3 +10,19 @@ if __name__ == "__main__":
         host="localhost",
         port=3306,
         user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities JOIN states ON cities.state_id = states.id "
+        "ORDER BY cities.id ASC"
+    )
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    conn.close()
